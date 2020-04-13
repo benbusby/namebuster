4cf7e3f64589ba7199df1233fac19ff34f42005d
Ben Busby <benbusby@pm.me>
2020-04-13 13:20:19 -0600

    Clarify module description in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ requirements = list(open('requirements.txt', 'r'))
 
 setuptools.setup(
     name='namebuster',
-    version='1.0.0',
+    version='1.0.1',
     scripts=['namebuster'] ,
     author='Ben Busby',
     author_email='contact@benbusby.com',
     install_requires=requirements,
-    description='A tool for generating a list of username permutations from a list of names, a file, or url, to use for brute forcing',
+    description='A tool for generating common username permutations from a list of names, a file, or url, to aid with username enumeration',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/benbusby/namebuster',
